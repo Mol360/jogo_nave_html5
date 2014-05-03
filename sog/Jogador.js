2e8f9377c926dfeab8c6c1df829ad13c67d5a9f5
Marcelo de Oliveira Lopes <mol360@gmail.com>
2014-05-03 13:52:00 -0300

    Movimentando sprite na tela

---
+++ sog/Jogador.js
@@ -0,0 +1,46 @@
+// Objeto do jogador.
+function Jogador(){
+    this.nave = new Nave();
+    this.pontos = 0;
+    this.ULTIMA_TECLA = {
+                espaco:false,
+                esquerda:false,
+                cima:false,
+                direita:false,
+                baixo:false
+              };
+    
+    this.Load = function(context,cWidth,cHeight){
+        this.context = context;
+        this.canvasWidth = cWidth;
+        this.canvasHeight = cHeight;
+        
+        this.nave.imagem = ARQUIVOS.nave_jogador;
+        this.nave.Load(context,this.canvasWidth,this.canvasHeight);
+    };
+    
+    this.pegaEntrada = function(){
+        if(TECLAS_ESTADO.esquerda && this.nave.estaVivo())
+            this.nave.irParaEsquerda();
+        if(TECLAS_ESTADO.direita && this.nave.estaVivo())
+            this.nave.irParaDireita();
+        if(TECLAS_ESTADO.cima && this.nave.estaVivo())
+            this.nave.irParaCima();
+        if(TECLAS_ESTADO.baixo && this.nave.estaVivo())
+            this.nave.irParaBaixo();
+        if(TECLAS_ESTADO.espaco && this.nave.estaVivo()){
+            this.nave.Atirar();
+            TECLAS_ESTADO.espaco = false;
+        }
+    };
+    this.estaVivo = function(){return this.nave.estaVivo();};
+    
+    this.Update = function(){
+        this.pegaEntrada();
+        this.nave.Update();
+    };
+    
+    this.Draw = function(){
+        this.nave.Draw();
+    };
+}
