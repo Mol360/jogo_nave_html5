68baf10de6daa30e7b373018f231989fe7c7eec4
Marcelo de Oliveira Lopes <mol360@gmail.com>
2014-05-11 20:18:35 -0300

    Implementando hug inicial. Implementando fim do jogo.

--- sog/Hud.js
+++ sog/Hud.js
@@ -12,6 +12,13 @@ function Hud() {
     this.vida = 0;
     this.tamanho_barra_vida = 250;
     
+    this.posicao_elementos = -200;
+    this.tamanho_texto = 40;
+    this.diferenca_tamanho_texto = 100;
+    this.aceleracao = 20;
+    this.evento = "Pausado";
+    this.tocarEvento = false;
+    
     
     this.Load = function(context,cWidth,cHeight){
         this.context = context;
@@ -26,6 +33,7 @@ function Hud() {
     
     this.Draw = function(){
         this.pegaBarraVida();
+        this.animarPause();
         this.pegaPontos();
     };
     
@@ -51,5 +59,52 @@ function Hud() {
 
         this.context.fillStyle = grd;
         this.context.fillRect(px,py,tamanho,altura_px);
+    };
+    
+    this.animarPause = function(){
+        if(this.tocarEvento && this.posicao_elementos+50<((this.canvasWidth/2)-this.diferenca_tamanho_texto)){
+            this.posicao_elementos +=0.5*this.aceleracao;
+        }else if(!this.tocarEvento && this.posicao_elementos+200>0){
+            this.posicao_elementos -=0.5*this.aceleracao;
+        }
+        
+        this.context.font = 'italic '+this.tamanho_texto+'pt Times Roman';
+        this.context.fillStyle = 'white';
+        this.context.lineWidth = 2;
+        this.context.strokeStyle = 'black';
+
+        this.context.strokeText(this.evento, this.posicao_elementos, this.canvasHeight/2);
+        this.context.fillText(this.evento, this.posicao_elementos, this.canvasHeight/2);
+    };
+    
+    this.Pausar = function(){
+        this.pausado = true;
+        this.evento = "Pausado";
+        this.diferenca_tamanho_texto = 50;
+        this.tocarEvento = true;
+    };
+    this.Continuar = function(){
+        this.pausado = false;
+        this.evento = "Começou";
+        this.diferenca_tamanho_texto = 50;
+        this.tocarEvento = false;
+    };
+    
+    this.fimDeJogo = function(){
+        this.evento = "Você Perdeu!";
+        this.diferenca_tamanho_texto = 100;
+        this.tocarEvento = true;
+    };
+    
+    this.comecar = function(){
+        this.evento = "Começar!";
+        this.diferenca_tamanho_texto = 50;
+        this.tocarEvento = true;
+    };
+    
+    this.venceuJogo = function(){
+        this.evento = "Parabéns você venceu!";
+        this.diferenca_tamanho_texto = 205;
+        this.tocarEvento = true;
     };
 }
